ae172e23912e792f0ed15b703d89a738154d4e40
Abhishek Balkrishna Raut <119692827+Abhishek4884@users.noreply.github.com>
2022-12-06 22:08:39 +0530

    Add files via upload

---
+++ Email_Attachment.py
@@ -0,0 +1,27 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+msg = MIMEMultipart()
+msg['from'] = 'rautabhishek4884@gmail.com'
+msg['to'] = 'rautabhishek4884@gmail.com'
+msg['subject'] = 'Attachment'
+body = "The mail contains Attchment"
+
+msg.attach(MIMEText(body , 'plain'))
+filename = 'Abhi.txt'
+Attchment = open('Abhi.txt' , 'rb')
+part = MIMEBase('application' , 'octet-stream')
+part.set_payload((Attchment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition' , "Attchment; filename= %s"%filename)
+msg.attach(part)
+
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()
+server.login('rautabhishek4884@gmail.com' , 'elnghmmqabcsvhoe')
+text = msg.as_string()
+server.sendmail('rautabhishek4884@gmail.com' , 'rautabhishek4884@gmail.com' ,text)
+server.quit()
